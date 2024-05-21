b4f6e38e21bdeae63f0a1596d9db5e9032b4e0a6
wsz <sz.wang@betterwood.com>
2024-05-21 18:02:21 +0800

    + tag 完成代码的使用问题

--- sunApp/entry/src/main/ets/entryability/EntryAbility.ts
+++ sunApp/entry/src/main/ets/entryability/EntryAbility.ts
@@ -11,16 +11,64 @@ export default class EntryAbility extends UIAbility {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
   }
 
-  onWindowStageCreate(windowStage: window.WindowStage) {
+  // onWindowStageCreate(windowStage: window.WindowStage) {
+  //   // Main window is created, set main page for this ability
+  //   hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+  //
+  //   windowStage.loadContent('pages/Splash/SplashPage', (err, data) => {
+  //     if (err.code) {
+  //       hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+  //       return;
+  //     }
+  //     hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+  //   });
+  // }
+
+  onWindowStageCreate(windowStage: window.WindowStage): void {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
+    //
+    // 1.获取应用主窗口。
+    let windowClass = null;
+    windowStage.getMainWindow((err, data) => {
 
-    windowStage.loadContent('pages/Splash/SplashPage', (err, data) => {
       if (err.code) {
-        hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
+        console.error('Failed to obtain the main window. Cause: ' + JSON.stringify(err));
         return;
       }
-      hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
+
+      windowClass = data;
+      console.info('Succeeded in obtaining the main window. Data: ' + JSON.stringify(data));
+
+
+      // 2.实现沉浸式效果：设置导航栏、状态栏不显示。
+      let names = ["status"];
+      windowClass.setWindowSystemBarEnable(names, (err) => {
+        if (err.code) {
+          console.error('Failed to set the system bar to be visible. Cause:' + JSON.stringify(err));
+          return;
+        }
+        console.info('Succeeded in setting the system bar to be visible.');
+      });
+      windowClass.setWindowLayoutFullScreen(true)
+
+    })
+
+    // windowStage.getMainWindow((err, window) => {
+    //   // window.setWindowSystemBarEnable(["navigation"])
+    //   window.getWindowProperties().isLayoutFullScreen = true
+    //   // window.setWindowSystemBarProperties({
+    //   //   statusBarColor: "#ffffff", isStatusBarLightIcon: false
+    //   // })
+    //   window.on("")
+    // })
+
+    windowStage.loadContent("pages/Splash/SplashPage", (err, data) => {
+      if (err.code) {
+        hilog.error(0x0000, 'testTag', '加载应用首页内容失败. Cause: %{public}s', JSON.stringify(err) ?? '');
+        return;
+      }
+      hilog.info(0x0000, 'testTag', '加载应用首页成功. Data: %{public}s', JSON.stringify(data) ?? '');
     });
   }
 
