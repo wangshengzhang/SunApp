8bbb2527e661e034e6a4c9be02c67807d6dc06dd
wsz <sz.wang@betterwood.com>
2024-05-24 15:56:25 +0800

    + tag 完成部分的数据的处理问题

--- sunApp/entry/src/main/ets/entryability/EntryAbility.ts
+++ sunApp/entry/src/main/ets/entryability/EntryAbility.ts
@@ -3,44 +3,29 @@ import hilog from '@ohos.hilog';
 import window from '@ohos.window';
 
 export default class EntryAbility extends UIAbility {
-  onCreate(want, launchParam) {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
-  }
 
-  onDestroy() {
+   onCreate(want, launchParam) {
+      hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onCreate');
+   }
+
+   onDestroy() {
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onDestroy');
-  }
+   }
 
-  // onWindowStageCreate(windowStage: window.WindowStage) {
-  //   // Main window is created, set main page for this ability
-  //   hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
-  //
-  //   windowStage.loadContent('pages/Splash/SplashPage', (err, data) => {
-  //     if (err.code) {
-  //       hilog.error(0x0000, 'testTag', 'Failed to load the content. Cause: %{public}s', JSON.stringify(err) ?? '');
-  //       return;
-  //     }
-  //     hilog.info(0x0000, 'testTag', 'Succeeded in loading the content. Data: %{public}s', JSON.stringify(data) ?? '');
-  //   });
-  // }
 
-  onWindowStageCreate(windowStage: window.WindowStage): void {
+   onWindowStageCreate(windowStage: window.WindowStage): void {
     // Main window is created, set main page for this ability
     hilog.info(0x0000, 'testTag', '%{public}s', 'Ability onWindowStageCreate');
     //
     // 1.获取应用主窗口。
     let windowClass = null;
     windowStage.getMainWindow((err, data) => {
-
       if (err.code) {
         console.error('Failed to obtain the main window. Cause: ' + JSON.stringify(err));
         return;
       }
-
       windowClass = data;
       console.info('Succeeded in obtaining the main window. Data: ' + JSON.stringify(data));
-
-
       // 2.实现沉浸式效果：设置导航栏、状态栏不显示。
       let names = ["status"];
       windowClass.setWindowSystemBarEnable(names, (err) => {
@@ -51,17 +36,8 @@ export default class EntryAbility extends UIAbility {
         console.info('Succeeded in setting the system bar to be visible.');
       });
       windowClass.setWindowLayoutFullScreen(true)
-
     })
 
-    // windowStage.getMainWindow((err, window) => {
-    //   // window.setWindowSystemBarEnable(["navigation"])
-    //   window.getWindowProperties().isLayoutFullScreen = true
-    //   // window.setWindowSystemBarProperties({
-    //   //   statusBarColor: "#ffffff", isStatusBarLightIcon: false
-    //   // })
-    //   window.on("")
-    // })
 
     windowStage.loadContent("pages/Splash/SplashPage", (err, data) => {
       if (err.code) {
